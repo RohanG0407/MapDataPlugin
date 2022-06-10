0464bb3ffc83a0e740b9819a7ee106b0b316b664
Rohan Gangaraju <gangaraju.rohan@gmail.com>
2022-06-10 11:59:25 -0400

    Add files via upload

---
+++ L.Control.MapInfo2.js
@@ -0,0 +1,20 @@
+L.Control.MapData = L.Control.extend({
+    options: {
+        currentCenter: 0,
+        currentZoom: 0
+    },
+
+    onAdd: function (map) {
+        map.on('move', function(e) {
+            this.currentCenter = map.getCenter()
+            $.post("/postMapData", {
+                data: "Hello"
+            });
+            console.log(map.getCenter())
+        });
+    }
+});
+
+L.control.mapData = function (options) {
+    return new L.Control.MapData(options);
+};
