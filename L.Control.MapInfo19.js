6fe148d366f346116838f7e745bc91ec5dbb7548
Rohan Gangaraju <gangaraju.rohan@gmail.com>
2022-06-14 14:17:52 -0400

    Add files via upload

---
+++ L.Control.MapInfo19.js
@@ -0,0 +1,41 @@
+L.Layer.MapData = L.Layer.extend({
+    onAdd: function (map) {
+        map.on('zoomstart', function () {
+            var zoomLevel = map.getZoom();
+            var tooltip = $('.leaflet-tooltip');
+        
+            switch (zoomLevel) {
+                case 2:
+                    tooltip.css('font-size', 5);
+                    break;
+                case 3:
+                    tooltip.css('font-size', 7);
+                    break;
+                case 4:
+                    tooltip.css('font-size', 10);
+                    break;
+                case 5:
+                    tooltip.css('font-size', 12);
+                    break;
+                case 6:
+                    tooltip.css('font-size', 14);
+                    break;
+                default:
+                    tooltip.css('font-size', 14);
+            }
+        });
+
+        map.on('move', function(e) {
+            currentCenter = map.getCenter();
+            currentZoom = map.getZoom();
+            data_dict = {
+                'currentCenter': [currentCenter.lat, currentCenter.lng],
+                'currentZoom': currentZoom
+            };
+
+            $.post("http://127.0.0.1:5000/postMapData", {
+                data: data_dict
+            });
+        });
+    }
+});
